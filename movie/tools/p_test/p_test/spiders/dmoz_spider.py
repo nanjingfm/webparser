8e4050c4edcb3a4a0dc03d9d47f60e33b4683824
fuming <fuming@pipapai.com>
2016-11-02 14:48:44 +0800

    crawlspider

--- movie/tools/p_test/p_test/spiders/dmoz_spider.py
+++ movie/tools/p_test/p_test/spiders/dmoz_spider.py
@@ -1,5 +1,10 @@
 import scrapy
 from p_test.items import DmozItem
+from scrapy import Selector
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spiders import CrawlSpider
+from scrapy.spiders import Rule
+
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
@@ -21,3 +26,26 @@ class DmozSpider(scrapy.Spider):
             items.append(item)
 
         return items
+
+class CSDNBlogCrawlSpider(CrawlSpider):
+    name = "CSDNBlogCrawlSpider"
+    allowed_domains = ['blog.csdn.net']
+    start_urls = ['http://blog.csdn.net/u012150179/article/details/11749017']
+    rules = [
+        Rule(SgmlLinkExtractor(allow=('/u012150179/article/details'),
+                               restrict_xpaths=('//li[@class="next_article"]')),
+             callback='parse_item',
+             follow=True)
+    ]
+
+    def parse_item(self, response):
+        # print "parse_item>>>>>>"
+        item = {}
+        sel = Selector(response)
+        blog_url = str(response.url)
+        blog_name = sel.xpath('//div[@id="article_details"]/div/h1/span/a/text()').extract()
+
+        item['blog_name'] = [n.encode('utf-8') for n in blog_name]
+        item['blog_url'] = blog_url.encode('utf-8')
+
+        yield item
